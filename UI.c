a0f8913f073e5af165ed75231948d57fc9a132be
Vincent Kengo <vincent.kengo@ha.ax>
2021-01-27 20:58:56 +0200

    Update UI.c
    enterCode go brrr

--- UI.c
+++ UI.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct date
 {
@@ -10,7 +11,7 @@ struct date
     int year;
 };
 
-bool isLeapYear (struct date d)
+bool isLeapYear(struct date d)
 {
     if ((d.year % 4 == 0 && d.year % 100 != 0) || d.year % 400 == 0)
         return true; // It's a leap year
@@ -18,7 +19,7 @@ bool isLeapYear (struct date d)
         return false; // Not a leap year
 }
 
-bool isInfected (int code)
+bool isInfected(int code)
 {
     int coronaCodes[3] = {24038623, 56509924, 15515512};
 
@@ -28,33 +29,33 @@ bool isInfected (int code)
         return false;
 }
 
-char * enterCode(int mode) // mode 0 = opening code. mode 1 = ID code
+char *enterCode(int mode) // mode 0 = opening code. mode 1 = ID code
 {
     static char code[9];
     bool validInput = false;
-    if (mode == 0)
-        printf("1. Input opening code (7 digits)\n");
-    else if (mode == 1)
-        printf("2.1. Enter ID code (8 digits)\n");
-    else
-        printf("Error in code: int mode has an invalid value.\n");
-    while(validInput == false)
+
+    while (validInput == false)
     {
-        fgets(code, 9 + mode, stdin);
+        fgets(code, 8 + mode, stdin);
         fflush(stdin);
         int i, count;
-        for(i = 0; code[i] != '\0'; i++)
+        for (i = 0; code[i] != '\0'; i++)
         {
-            if( isdigit(code[i]) == true)
+            if (isdigit(code[i]) == true)
                 count++;
+            else
+            {
+                printf("Invalid input >:(\n");
+                count = 0;
+                memset(code, 0, 9*sizeof(char));
+            }
+            
         }
-        if(count == 7 + mode)
+        if (count == 7 + mode)
             validInput = true;
         else
-        {
-            printf("Invalid input >:(\n");
             count = 0;
-        }
+        
     }
     return code;
 }
@@ -63,32 +64,27 @@ bool dateValidator(struct date d)
 {
     const int daysPerMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    if(d.month > 12 || d.month < 1)
+    if (d.month > 12 || d.month < 1)
         return false;
-    if(d.month == 2 && d.day == 29 && (isLeapYear(d) == false) )
+    if (d.month == 2 && d.day == 29 && (isLeapYear(d) == false))
         return false;
-    if(d.day > daysPerMonth[d.month] || d.day < 1)
+    if (d.day > daysPerMonth[d.month] || d.day < 1)
         return false;
-    if(d.year < 1 || d.year > 3000)
+    if (d.year < 1 || d.year > 3000)
         return false;
     return true;
 }
-int Quit = 0;
-    while (Quit == 0)
-    {
-        printf("3. Type 3 to confirm exit: ");
-        scanf("%d", &Quit);
 
 struct date enterDate()
 {
     printf("2. Input date (DD.MM.YYYY)\n");
     struct date d;
-    while(1)
+    while (1)
     {
         scanf("%d.%d.%d", &d.day, &d.month, &d.year);
-        
+        fflush(stdin);
 
-        if(dateValidator(d) == false)
+        if (dateValidator(d) == false)
             printf("Entered invalid date. Try again\n");
         else
             return d;
@@ -98,10 +94,14 @@ struct date enterDate()
 int main(void)
 {
     struct date d;
-    printf ("3. Exit program\n");
-    const char * code1 = enterCode(0);
-    printf("The code is: %s",code1);
+
+    const char *id = enterCode(0);
+    printf("%s\n",id);
+    fflush(stdin);
     d = enterDate();
-    printf("The date is: %i.%i.%i", d.day, d.month, d.year);
+    printf("%d.%d.%d\n", d.day, d.month, d.year);
+    const char *code = enterCode(1);
+    printf("%s\n",code);
+
     return 0;
 }
