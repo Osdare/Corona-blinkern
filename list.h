5e0a8a4dc23366481c6e7fe9391c3df0e8f85f03
Vincent Kengo <vincent.kengo@ha.ax>
2021-03-03 21:14:07 +0200

    list.h grej

---
+++ list.h
@@ -0,0 +1,17 @@
+#ifndef __LIST_H__
+#define __LIST_H__
+
+struct date;
+struct node;
+
+struct node * createList(void);
+void destroyList(struct node* h);
+struct node *newRegistration(struct node *ptr);
+int lengthList(struct node * h);
+void printothersMet(struct node *h);
+void printIDs(struct node *h);
+void meeting(struct node *p1, struct node *p2);
+int countLeapYears(struct date d);
+int dateDifference(struct date d1, struct date d2);
+void validateMeetingDates(struct node *h);
+void checkInfected(struct node *h);
